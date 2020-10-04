18b1e02e1cf2501d30cb0bb477a54faf9696f5e9
Anastasia Shpit <imshpit@gmail.com>
2020-10-05 02:20:11 +0300

    adde user model

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const cartRoutes = require('./routes/cart');
 const { countReset } = require('console');
 const Handlebars = require('handlebars');
 const {allowInsecurePrototypeAccess} = require('@handlebars/allow-prototype-access');
+const User = require('./models/user');
 
 const app = express();
 
@@ -22,6 +23,16 @@ const hbs = exphbs.create({
 app.engine('hbs', hbs.engine);
 app.set('view engine', 'hbs'); 
 app.set('views', 'views');
+
+app.use(async(req, res, next) => {
+    try {
+        const user = await User.findById("5f7a503fb2476643d8766a6c");
+        req.user = user;
+        next();
+    } catch (e) {
+        console.log(e);
+    }
+});
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({extended: true}));
@@ -40,6 +51,15 @@ async function start() {
             useUnifiedTopology: true,
             useFindAndModify: false
         });
+        const candidate = await User.findOne();
+        if(!candidate) {
+            const user = new User({
+                email: 'imshpit@gmail.com',
+                name: 'Nastya',
+                cart: {items: []}
+            });
+            await user.save();
+        }
         app.listen(PORT, () => {
             console.log(`Server is running on port ${PORT}`);
         });
--- routes/cart.js
+++ routes/cart.js
@@ -1,12 +1,10 @@
 const {Router} = require('express');
-const { route } = require('./add');
 const router = Router();
-const Cart = require('../models/cart');
 const Course = require('../models/course');
 
 router.post('/add', async (req, res) => {
-    const course = await Course.getById(req.body.id);
-    await Cart.add(course);
+    const course = await Course.findById(req.body.id);
+    await req.user.addToCart(course);
     res.redirect('/cart');
 });
 
@@ -16,13 +14,15 @@ router.delete('/remove/:id', async (req, res) => {
 })
 
 router.get('/', async (req, res) => {
-    const cart = await Cart.fetch();
-    res.render('cart', {
-        title: 'Корзина',
-        isCart: true,
-        courses: cart.courses,
-        price: cart.price
-    });
+    // const cart = await Cart.fetch();
+    // res.render('cart', {
+    //     title: 'Корзина',
+    //     isCart: true,
+    //     courses: cart.courses,
+    //     price: cart.price
+    // });
+
+    res.json({test: true});
 });
 
 module.exports = router;
