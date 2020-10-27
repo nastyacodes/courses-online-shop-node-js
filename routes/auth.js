7baec6e64ce1a84d5b6b49d97897e24c13b04160
Anastasia Shpit <imshpit@gmail.com>
2020-10-27 23:35:27 +0200

    Added a page for changing user password

--- routes/auth.js
+++ routes/auth.js
@@ -93,6 +93,32 @@ router.get('/reset', (req, res) => {
     });
 });
 
+router.get('/password/:token', async (req, res) => {
+    if(!req.params.token) {
+        return res.redirect('/auth/login');
+    }
+
+    try {
+        const user = await User.findOne({
+            resetToken: req.params.token,
+            resetTokenExp: {$gt: Date.now()}
+        });
+
+        if (!user) {
+            return res.redirect('/auth/login');
+        } else {
+            res.render('auth/password', {
+                title: 'Восстановить доступ',
+                error: req.flash('error'),
+                userId: user._id.toString(),
+                token: req.params.token
+            });   
+        }
+    } catch (e) {
+        console.log(e);
+    }
+});
+
 router.post('/reset', (req, res) => { //async тут не нужен!
     try {
         crypto.randomBytes(32, async (err, buffer) => {
@@ -119,6 +145,29 @@ router.post('/reset', (req, res) => { //async тут не нужен!
     } catch (e) {
         console.log(e);
     }
-})
+});
+
+router.post('/password', async (req, res) => {
+    try {
+        const user = await User.findOne({
+            _id: req.body.userId,
+            resetToken: req.body.token,
+            resetTokenExp: {$gt: Date.now()}
+        });
+
+        if (user) {
+            user.password = await bcrypt.hash(req.body.password, 10);
+            user.resetToken = undefined;
+            user.resetTokenExp = undefined;
+            await user.save();
+            res.redirect('/auth/login');
+        } else {
+            req.flash('loginError', 'Время жизни токена истекло!');
+            res.redirect('/auth/login');
+        }
+    } catch (e) {
+        console.log(e);
+    }
+});
 
 module.exports = router;
